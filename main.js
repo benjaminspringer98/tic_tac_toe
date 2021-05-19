564da6640227c23b17cfa3f68fc711d220116d24
Benjamin Springer <benjamin.springer98@gmail.com>
2021-05-19 16:00:00 +0200

    added basic logic for displaying the player's symbol on the game board

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
 // modules: 
-const Gameboard = (function() {
+const Gameboard = (() => {
     let gameboardArray = [];
 
     function getGameboardArray() {
         return gameboardArray;
     }
 
-return {
-    getGameboardArray
+    function addToGameboardArray(symbol, position) {
+        gameboardArray[position] = symbol;
+    }
+
+    return {
+        getGameboardArray,
+        addToGameboardArray
 }
 })(); 
+
+const GameController = (() => {
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        Gameboard.addToGameboardArray("X", box.getAttribute("data"));
+        DisplayController.addToScreen("X", box);
+    });
+});
+})();
+
+const DisplayController = (() => {
+    
+    function addToScreen(symbol, box) {
+        box.textContent = symbol;
+    }
+
+    return {
+        addToScreen
+    }
+})();
+
+
+// objects:
+const Player = () => {
+
+    return {
+
+    }
+};
 
 
 
